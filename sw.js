fb5ba091a147e317267a93392e7ecd312b8fa483
zenobrasil2025-glitch <zenobrasil2025@gmail.com>
2025-09-03 21:26:01 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
 importScripts("https://cdn.brevo.com/js/sdk-loader.js");
+
 Brevo.push([
     "init",
     {
-        client_key: (location.search.match(/[?&]key=([^&]*)/) || [])[1],
+        client_key: "eyJhcGlfa2V5IjoieGtleXNpYi0wOThmNmIxZGM0YWJkYzVjNDFkODg5OTY5OGM4ZDRlY2M1OTllNWYzZmQ1ZjQyY2FlNzUxYmNhNzYyZGE1NjVmLUQyRWYwUGtHZEFDVGVvNlAifQ==",
     },
 ]);
