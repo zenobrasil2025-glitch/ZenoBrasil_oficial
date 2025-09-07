86d106cd0eabf2b3a83e9acc46d7c2ed26a4853b
zenobrasil2025-glitch <zenobrasil2025@gmail.com>
2025-09-07 16:12:13 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-importScripts("https://cdn.brevo.com/js/sdk-loader.js");
-
-Brevo.push([
-    "init",
-    {
-        client_key: "eyJhcGlfa2V5IjoieGtleXNpYi0wOThmNmIxZGM0YWJkYzVjNDFkODg5OTY5OGM4ZDRlY2M1OTllNWYzZmQ1ZjQyY2FlNzUxYmNhNzYyZGE1NjVmLUQyRWYwUGtHZEFDVGVvNlAifQ==",
-    },
-]);
